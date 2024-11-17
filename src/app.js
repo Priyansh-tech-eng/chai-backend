1581577140dae2d6b6ebf786e7423f03c69e2a4d
priyanshu <your.email@example.com>
2024-11-17 13:57:56 +0530

    all setup configure

--- src/app.js
+++ src/app.js
@@ -15,4 +15,11 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declerstion
+app.use("/api/v1/users", userRouter)
+
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,42 +1,48 @@
-// require('dotenv').config()
+// require('dotenv').config(path: './env')
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from './app.js'
+
+// import express from "express"
+// const app = express()
+
 
 dotenv.config({
-  path: './env'
+    path: './env'
 })
-
-
-
 
 connectDB()
 .then(() => {
-  app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running at port : ${process.env.PORT}`);
-  })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port : ${process.env.PORT}`);
+    
 })
+
+})
+
 .catch((err) => {
-  console.log("Mongo db connection falid !!!", err);
+    console.log("MONGO db connection failed !!!", err);
 })
+
+
+
 
 
 /*
-import express from "express"
-const app = express()
-(async() => {
+( async () => {
     try {
-      await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-      app.on("errror", (error) => {
-        console.log("ERRR: ", error);
-        throw error 
-      })
+       await mongoose.connect(`$(process.env.MONGODB_URI)/${DB_NAME}`)
+       app.on("errror", (error) => {
+           console.log("ERRR:", error);
+           throw error
+       })
 
-      app.listen(prosses.env.PORT, () => {
-        console.log(`App is listening on port port $ {process.env.PORT}`)
-      })
+       app.listen(process.env.PORT, () => {
+        console.log(`App is listing on port ${process.env.PORT}`)
+       })
     } catch (error) {
-        console.error("ERROR:", error)
-        throw err
+       console.error("ERROR:", error)
+       throw err
     }
 })()
     */
